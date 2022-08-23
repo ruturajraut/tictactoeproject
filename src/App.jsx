16e7bb1b949290d8e347a4f6b4c841633cf04d96
Ruturaj GitHub <ruturajraut2000@gmail.com>
2022-08-23 13:05:20 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App=()=>{
+  return(
+  <>
+    <h1>Welcome to React Vite App!</h1>
+    <p>Ruturaj Aadiraj Pappa Aai</p>
+    <small>Hello This is Ruturaj</small>
+  </>
+  );
+};
+
+export default App;
